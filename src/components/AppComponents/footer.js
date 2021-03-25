46f6ef9131205592eee1aec5b883b7ce3485b231
kumarvikramshahi <63746254+kumarvikramshahi@users.noreply.github.com>
2021-03-25 17:40:27 +0530

    added feedback, github repo, and mail button in footer.js

--- src/components/AppComponents/footer.js
+++ src/components/AppComponents/footer.js
@@ -7,10 +7,31 @@ export default class Footer extends React.Component {
             marginTop: "10%",
             textAlign: "center",
         }
+        let button2 = {
+            border:"1px solid transparent",
+            backgroundColor:"black",
+            color:"white",
+            padding:"10px",
+            width:"50px",
+            borderRadius:"5px",
+            cursor:"pointer",
+            marginTop:"3px"
+        }
+        let button ={
+            border:"1px solid transparent",
+            backgroundColor:"black",
+            color:"white",
+            padding:"10px",
+            borderRadius:"5px",
+            cursor:"pointer",
+            fontWeight:"600",
+            marginTop:"3px"
+        }
         return (
             <footer style={myStyle}>
-                if you have any suggestions regarding this page or have matrials like "Notes" then you can mail it on <a href="mailto:lightscience29@gmail.com" target="_blank" rel="noopener noreferrer"> lightscience29@gmail.com </a><br />
-                <b><i>no copyrights of this webpage you can copy it.</i></b>
+                <a href="mailto:lightscience29@gmail.com" target="_blank" rel="noopener noreferrer"><button style={button}>Feedback</button></a><br/>
+                <a href="https://github.com/kumarvikramshahi/forcucekians.github.io" target="_blank" rel="noopener noreferrer"><button style={button}>Github Repo</button></a><br/>
+                if you have any suggestions regarding this page or have matrials like "Notes" or previous year Question papers or wanna request any material then click on below mail button. <br/><a href="mailto:lightscience29@gmail.com" target="_blank" rel="noopener noreferrer"> <button style={button2}>Mail</button> </a> <br />
                 <br /><br /><br /><br />
             </footer>
         )
