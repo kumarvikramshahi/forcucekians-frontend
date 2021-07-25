088a7382bfb7eaee71711f9bff1274f755243752
kumarvikramshahi <63746254+kumarvikramshahi@users.noreply.github.com>
2021-07-25 15:18:13 +0530

    deleted unused files Background.js,.css

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 
 ReactDOM.render(
-  //make sure to avoid use of HashRouter, using it will disturb different functionalty. 
-  <BrowserRouter basename="/">
-    <App />
-  </BrowserRouter>
+  //make sure to avoid use of HashRouter, using it will disturb different functionalty.
+  <React.StrictMode>
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
   , document.getElementById("root")
 )
