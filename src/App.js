ccaa9dda6b18ab4e5a1ec7991016b2045129d38a
kumarvikramshahi <63746254+kumarvikramshahi@users.noreply.github.com>
2021-12-31 04:26:58 +0530

    added env based server link

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Footer from "./components/AppComponents/footer"
 import ToTop from "./components/AppComponents/ToTop"
 import "./App.css"
 
-window.$SERVER_URI = 'https://forcucekians.herokuapp.com';
-// window.$SERVER_URI = 'http://localhost:8080';
+if (process.env.NODE_ENV === "production")
+    window.$SERVER_URI = 'https://forcucekians.herokuapp.com';
+if (process.env.NODE_ENV === "development")
+    window.$SERVER_URI = 'http://localhost:8080';
+
 window.$TOKEN = localStorage.getItem("bhaagB**");
 window.$USER_ID = localStorage.getItem("userId");
 window.$IsAuth = window.$TOKEN && window.$USER_ID;
